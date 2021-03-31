be2e8e65dbabee4147a0dd781e1379fed7e2714f
jensm04 <mail@ditisjens.be>
2021-03-31 19:24:48 +0200

    Changed static items to variables.

--- serverside_script.sh
+++ serverside_script.sh
@@ -2,6 +2,11 @@
 # Setting the port speed
 stty -F /dev/ttyACM0 raw speed 9600
 
+# login details for idrac server
+IDRACIP="idrac-esx02.maes.local"
+IDRACUSER="test"
+IDRACPASS="test"
+
 while true;
 do
         read result < /dev/ttyACM0 
@@ -9,10 +14,10 @@ do
         case $EVAL in
         START)
                 echo "Received start, setting fan speed to 0x32"
-		ipmitool -I lanplus -H idrac-esx02.maes.local -U test -P test raw 0x30 0x30 0x02 0xff 0x32;;
+		ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASS raw 0x30 0x30 0x02 0xff 0x32;;
 	STOP)
 		echo "Received stop, setting fan speed to 0xF"
-		ipmitool -I lanplus -H idrac-esx02.maes.local -U test -P test raw 0x30 0x30 0x02 0xff 0xF;;
+		ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASS raw 0x30 0x30 0x02 0xff 0xF;;
 	*)
 		echo "Oops...";;
         esac
@@ -20,6 +25,6 @@ done &
 
 while true;
 do
-	ipmitool -I lanplus -H idrac-esx02.maes.local -U test -P test sdr type temperature | grep 0Eh | cut -d"|" -f5 | cut -d" " -f2 > /dev/ttyACM0
+	ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASS sdr type temperature | grep 0Eh | cut -d"|" -f5 | cut -d" " -f2 > /dev/ttyACM0
 	sleep 10;
 done
