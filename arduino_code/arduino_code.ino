8b5d1a3227c86de3bcf4018a549487eb2f81de0d
jensm04 <mail@ditisjens.be>
2021-05-19 18:16:06 +0200

    Everything is working

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,10 +1,13 @@
 /*
- * IRremote: IRrecvDemo - demonstrates receiving IR codes with IRrecv
- * An IR detector/demodulator must be connected to the input RECV_PIN.
- * Version 0.1 July, 2009
- * Copyright 2009 Ken Shirriff
- * http://arcfn.com
+ * RoomADM
+ * A project by Jens Maes & Jonas Lembrechts
+ * Arduino: Arduino UNO
+ * IR remote: LAFVIN
  */
+#include <dht.h>
+#include <IRremote.h>
+#include <LiquidCrystal_I2C.h>
+
 #define IR_UP 0x46
 #define IR_DOWN 0x15
 #define IR_RIGHT 0x43
@@ -22,10 +25,21 @@
 #define IR_0 0x52
 #define IR_HASHTAG 0x4A
 #define IR_STAR 0x42
+#define LED_RED 10 
+#define LED_BLUE 11 
+#define LED_GREEN 12 
+#define LED_YELLOW 6
+
+/* Sound sensor */
+#define SSPIN A0
+
+/* Temp sensor */
+#define DHT11_PIN 7
 
 
-#include <IRremote.h>
-#include <LiquidCrystal_I2C.h>
+
+dht DHT;
+
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 int RECV_PIN = 8;
@@ -33,38 +47,115 @@ int RECV_PIN = 8;
 decode_results results;
 
 unsigned long result_value;
+bool sentTempWarning = false;
 
 void setup()
 {
-  Serial.begin(9600);
-  // In case the interrupt driver crashes on setup, give a clue
-  // to the user what's going on.
-  Serial.println("Enabling IRin");
   IrReceiver.begin(RECV_PIN, ENABLE_LED_FEEDBACK);
-  Serial.println("Enabled IRin");
-  Serial.println("Enabling screen");
   lcd.init();
   lcd.clear();
   lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Placeholder");
+  pinMode(LED_RED,OUTPUT);
+  pinMode(LED_BLUE,OUTPUT);
+  pinMode(LED_GREEN,OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
+  remoteControl();
+  getRoomTemp(); 
+  getRoomHumid();  
+  soundSensor();
+  serialMonitor();
+}
 
+void changeFanSpeed(String server, String fspeed) {
+  Serial.println(server+fspeed+"_");
+}
+
+void serialMonitor() {
   if (Serial.available() > 0) {
     lcd.clear();
     String temp = Serial.readStringUntil('\n');
-<<<<<<< HEAD
-    lcd.print("Temp: "+temp+" C");
-=======
     String cpuID, cpuTemp;
     cpuID = temp.substring(3,4);
     cpuTemp = temp.substring(4,7);
-    lcd.print("S"+cpuID+": "+cpuTemp+" C");
->>>>>>> c331198b80ffeba4d6a5465fcec143cc90c446d8
-    delay(100);
+    lcd.setCursor(0,0);
+    lcd.print("Server "+cpuID);
+    lcd.setCursor(0,1);
+    lcd.print("Temp: "+cpuTemp+" C");
+    delay(10000);
+    lcd.clear();
   }
+}
 
-  if (IrReceiver.decode()) {
+void getRoomTemp() {
+  int chk = DHT.read11(DHT11_PIN);
+  int temperature = DHT.temperature;
+
+  lcd.setCursor(0,0);
+  lcd.print("Temp: ");
+  lcd.print(DHT.temperature);
+  lcd.print((char)223);
+  lcd.print("C");
+
+  if(temperature > 25 && temperature < 45) {
+    statusRed();
+    if(!sentTempWarning) {
+      Serial.println("MAILTEMP_");
+    }
+    sentTempWarning = true;
+  } else {
+    statusGreen();
+    sentTempWarning = false;
+  }
+}
+
+void getRoomHumid() {
+  int chk = DHT.read11(DHT11_PIN);
+
+  lcd.setCursor(0,1);
+  lcd.print("Humidity: ");
+  lcd.print(DHT.humidity);
+  lcd.print("%");
+}
+
+
+void statusGreen() {
+  digitalWrite(LED_GREEN,HIGH);
+  digitalWrite(LED_RED,LOW);
+  digitalWrite(LED_BLUE,LOW);
+}
+
+void statusRed() {
+  digitalWrite(LED_GREEN,LOW);
+  digitalWrite(LED_RED,HIGH);
+  digitalWrite(LED_BLUE,LOW);
+}
+
+void statusBlue() {
+}
+
+void soundSensor() {
+  int tmp = analogRead(A0);
+
+  if(tmp > 720) {
+    digitalWrite(LED_YELLOW,HIGH);
+  } else {
+    digitalWrite(LED_YELLOW,LOW);
+  }
+  
+}
+
+void getTemp(String server) {
+  Serial.println(server+"GET_");
+  lcd.print("Temperature:");
+}
+
+void remoteControl() {
+if (IrReceiver.decode()) {
     int result = IrReceiver.decodedIRData.command;
     delay(100);
     switch (IrReceiver.decodedIRData.command) {
@@ -82,14 +173,25 @@ void loop() {
             switch(IrReceiver.decodedIRData.command) {
               case IR_UP:
                 changeFanSpeed("S1","0xA");
+                IrReceiver.resume(); // Receive the next value
+                lcd.clear();
+                lcd.print("DFC enable S1"); 
+                delay(5000);
+                lcd.clear();
                 break;
               case IR_DOWN:
                 changeFanSpeed("S1","0xB");
+                IrReceiver.resume(); // Receive the next value 
+                lcd.clear();
+                lcd.print("DFC disable S1"); 
+                delay(5000);
+                lcd.clear();
                 break;
             }
             break;
         }
         break;
+        IrReceiver.resume(); // Receive the next value 
       case IR_STAR:
         IrReceiver.resume();
         while(!IrReceiver.decode()) {
@@ -97,28 +199,14 @@ void loop() {
         }
         switch(IrReceiver.decodedIRData.command) {
           case IR_1:
-            getSpeed("S1");
+            getTemp("S1");
+            IrReceiver.resume();
             break;
         }
-    IrReceiver.resume(); // Receive the next value 
+        IrReceiver.resume(); // Receive the next value 
+        break;
     }
-<<<<<<< HEAD
-
-    IrReceiver.resume(); // Receive the next value
-
-=======
->>>>>>> c331198b80ffeba4d6a5465fcec143cc90c446d8
+    lcd.clear();
+    IrReceiver.resume();
   }
-}
-
-void changeFanSpeed(String server, String fspeed) {
-  Serial.println(server+"$"+fspeed+"_");
-}
-
-void getRoomTemp() {
-  
-}
-
-void getSpeed(String server) {
-  Serial.println(server+"GET_");
 }
--- serverside_script.sh
+++ serverside_script.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 # Setting the port speed
-ARDUINOPORT=/dev/ttyACM1
+clear
+ARDUINOPORT=/dev/ttyACM0
 
-stty -F $ARDUINOPORT raw speed 9600
+sudo stty -F $ARDUINOPORT raw speed 9600
+sudo stty -F $ARDUINOPORT -hupcl
 
 # login details for idrac server
 IDRACIP="idrac-esx02.maes.local"
 IDRACUSER="test"
 IDRACPASS="test"
 
-while true;
-do
-    read result < $ARDUINOPORT
-	EVAL=$(echo $result | cut -f1 -d"_")
-    case $EVAL in
-		S1UP)
-			echo "Received stop, setting fan speed to 0xF"
-			ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASS raw 0x30 0x30 0x02 0xff 0xF;;
-			read result < $ARDUINOPORT
-			EVAL=$(echo $result | cut -f1 -d"_")
-		S1DOWN)
-			echo "Received stop, setting fan speed to 0xF"
-			ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASS raw 0x30 0x30 0x02 0xff 0xF;;
-		*)
-			echo "Oops...";;
-			esac
-done &
+sleep 10s;
+echo "ready for data"
 
 while true;
 do
-	T1=$(ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASS sdr type temperature | grep 0Eh | cut -d"|" -f5 | cut -d" " -f2)
-	T2=$(ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASS sdr type temperature | grep 0Fh | cut -d"|" -f5 | cut -d" " -f2)
-	echo "CPU1$T1" > $ARDUINOPORT
-	sleep 5;
-	echo "CPU2$T2" > $ARDUINOPORT
-	sleep 10;
+        read result < $ARDUINOPORT
+	EVAL=$(echo $result | cut -f1 -d"_")
+        case $EVAL in
+        S10xA)
+                echo "Received DFC on"
+		ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASS raw 0x30 0x30 0x01 0x01;;
+	S10xB)
+		echo "Received DFC off, setting static speed"
+		ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASS raw 0x30 0x30 0x01 0x00
+		ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASS raw 0x30 0x30 0x02 0xff 0xA;;
+	S1GET)
+                echo "Asking for S1 status"
+		T1=$(ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASS sdr type temperature | grep 0Eh | cut -d"|" -f5 | cut -d" " -f2)
+		echo "CPU1$T1" > $ARDUINOPORT;;
+        MAILTEMP)
+		echo "Received temperature warning"
+		echo "Temperature warning received. Please check your datacenter." | mail -s "[WARNING] Temperature" mail@ditisjens.be;;
+	esac
 done
+
+
